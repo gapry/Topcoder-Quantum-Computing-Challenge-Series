5262068b3f9ebb1053a64a1104dff988a4914be4
gapry <gapry@protonmail.com>
2019-02-25 21:47:23 +0800

    [scheduling] update runscript

--- 02-scheduling/run.sh
+++ 02-scheduling/run.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 declare -r py=/usr/bin/python3.7
+declare -r src=main.py
 
 function fmt() {
   yapf -i --style='{
     based_on_style: pep8, 
     indent_width: 2, 
     blank_lines_around_top_level_definition: 1
-  }' main.py
+  }' $src
 }
 
 function init() {
@@ -16,12 +17,18 @@ function init() {
 }
 
 function execute() {
-  $py main.py
+  $py $src
+}
+
+function pre_submit() {
+  rm -f *.zip
+  zip -r scheduling.zip $src
 }
 
 function main() {
   init
   execute
+  pre_submit
 }
 
 main
